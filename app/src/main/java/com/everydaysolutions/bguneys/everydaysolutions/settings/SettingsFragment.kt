abf1fa2ee0b0b0189d839e407b126533e3143ac2
bguneys <42909556+bguneys@users.noreply.github.com>
2020-05-31 14:00:59 +0300

    Share app and rate app sections added in Settings.

--- app/src/main/java/com/everydaysolutions/bguneys/everydaysolutions/settings/SettingsFragment.kt
+++ app/src/main/java/com/everydaysolutions/bguneys/everydaysolutions/settings/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package com.everydaysolutions.bguneys.everydaysolutions.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.core.app.ShareCompat
 import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -9,9 +12,7 @@ import com.everydaysolutions.bguneys.everydaysolutions.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     /**
-     * Called during [.onCreate] to supply the preferences for this fragment.
-     * Subclasses are expected to call [.setPreferenceScreen] either
-     * directly or via helper methods such as [.addPreferencesFromResource]
+     * Called during [.onCreate] to supply the preferences for this fragment
      */
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -19,17 +20,36 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
 
-        if (preference?.key.equals(getString(R.string.preference_disclaimer))) {
-            findNavController().navigate(R.id.action_action_settings_to_disclaimerFragment)
-        }
+        when(preference?.key) {
 
-        if (preference?.key.equals(getString(R.string.preference_about))) {
-            //Navigate to DisclaimerFragment
-            findNavController().navigate(R.id.action_action_settings_to_aboutFragment)
+            getString(R.string.preference_disclaimer) -> {
+                findNavController().navigate(R.id.action_action_settings_to_disclaimerFragment)
+            }
+
+            getString(R.string.preference_about) -> {
+                findNavController().navigate(R.id.action_action_settings_to_aboutFragment)
+            }
+
+            getString(R.string.share_app) -> {
+                //share app Play Store page link
+                ShareCompat.IntentBuilder
+                    .from(requireActivity())
+                    .setType("text/plain")
+                    .setChooserTitle(getString(R.string.share_this_app_with))
+                    .setText("https://play.google.com/store/apps/details?id=" + requireActivity().packageName)
+                    .startChooser()
+            }
+
+            getString(R.string.rate_app) -> {
+                //start app Play Store page for rating
+                val url = "https://play.google.com/store/apps/details?id=" + requireActivity().packageName
+                val uri = Uri.parse(url)
+                val rateIntent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(rateIntent)
+            }
         }
 
         return super.onPreferenceTreeClick(preference)
     }
-
 
 }
